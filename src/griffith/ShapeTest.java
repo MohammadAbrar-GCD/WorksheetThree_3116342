f67be78b6a797ac6d7ef50cbd8e90aadddb001c7
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-03-16 15:27:30 +0000

    Unit test for Circle perimeter and toString

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -12,7 +12,14 @@ class ShapeTest {
 	    public void testCircle() {
 		 Circle circle = new Circle("Circle", 5.0);
 	        
+		 
 		 assertEquals(78.53981633974483, circle.area(), 0.0001);
+		 
+		 assertEquals(31.41592653589793, circle.perimeter(), 0.0001);
+		 
+		 assertEquals("Shape: Circle, Radius: 5.0", circle.toString()); 
 	 }
+	 
+	 
 
 }
