23c7dcbe651ff156a749fcfc739f6dc309c1f224
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-03-16 14:10:49 +0000

    Add abstract shape class

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -2,6 +2,26 @@ package griffith;
 
 public abstract class Shape {
 	
-	
+	private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return "Shape: " + name;
+    }
 
 }
