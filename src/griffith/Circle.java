547a02f5287a6de00392cbbab93a6fe3dfd25823
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-03-16 14:23:28 +0000

    added the circle class

--- src/griffith/Circle.java
+++ src/griffith/Circle.java
@@ -16,16 +16,19 @@ public class Circle extends Shape{
         this.radius = radius;
     }
 
+ // Overriding the abstract method to calculate the area of the circle
     @Override
     public double area() {
-        return Math.PI * radius * radius;
+        return Math.PI * radius * radius; // Area of a circle is π r^2
     }
 
+    // Overriding the abstract method to calculate the perimeter 
     @Override
     public double perimeter() {
-        return 2 * Math.PI * radius;
+        return 2 * Math.PI * radius; // Perimeter  is 2πr
     }
 
+    // Overriding toString to include radius
     @Override
     public String toString() {
         return super.toString() + ", Radius: " + radius;
