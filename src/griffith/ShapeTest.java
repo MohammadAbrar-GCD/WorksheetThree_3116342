9533464996b78d14a15ce76147573374d8232055
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-03-17 14:56:24 +0000

    Updated testRhobus area method

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -20,12 +20,12 @@ class ShapeTest {
 		 assertEquals("Shape: Circle, Radius: 5.0", circle.toString()); 
 	 }
 	 
+	 @Test
 	 public void testRhombus() {
 		    
 		    Rhombus rhombus = new Rhombus("Rhombus", 6.0, 8.0);
 		    
-		    assertEquals(30.0, rhombus.area(), 0.0001);
-		    
+		    assertEquals(24.0, rhombus.area(), 0.0001); 
 		    
 	 }
 
